3a073eddc7b1113cb2c50e4c6d541c6d8c7cec44
LNam14 <lehoainam2506@gmail.com>
2024-08-17 00:46:26 +0700

    Fix useSearchParams

--- src/app/(public)/(auth)/logout/page.tsx
+++ src/app/(public)/(auth)/logout/page.tsx
@@ -1,26 +1,35 @@
-'use client'
-import { getRefreshTokenFormLocalStorage } from '@/lib/utils'
-import { useLogoutMutation } from '@/queries/useAuth'
-import { useRouter, useSearchParams } from 'next/navigation'
-import React, { useEffect, useRef } from 'react'
+'use client';
+
+import { getRefreshTokenFormLocalStorage } from '@/lib/utils';
+import { useLogoutMutation } from '@/queries/useAuth';
+import { useRouter, useSearchParams } from 'next/navigation';
+import React, { useEffect, useRef, Suspense } from 'react';
+
+function LogoutComponent() {
+    const { mutateAsync } = useLogoutMutation();
+    const router = useRouter();
+    const searchParams = useSearchParams();
+    const refreshTokenFromUrl = searchParams.get('refreshToken');
+    const ref = useRef<any>(null);
+
+    useEffect(() => {
+        if (ref.current || refreshTokenFromUrl !== getRefreshTokenFormLocalStorage()) return;
+        ref.current = mutateAsync;
+        mutateAsync().then((res) => {
+            setTimeout(() => {
+                ref.current = null;
+            }, 1000);
+            router.push('/login');
+        });
+    }, [mutateAsync, router, refreshTokenFromUrl]);
+
+    return <div>Logging out...</div>;
+}
 
 export default function LogoutPage() {
-    const { mutateAsync } = useLogoutMutation()
-    const router = useRouter()
-    const searchParams = useSearchParams()
-    const refreshTokenFromUrl = searchParams.get('refreshToken')
-    const ref = useRef<any>(null)
-    useEffect(() => {
-        if (ref.current || refreshTokenFromUrl !== getRefreshTokenFormLocalStorage()) return
-        ref.current = mutateAsync
-        mutateAsync().then(res => {
-            setTimeout(() => {
-                ref.current = null
-            }, 1000)
-            router.push('/login')
-        })
-    }, [mutateAsync, router, refreshTokenFromUrl])
     return (
-        <div>LogoutPage</div>
-    )
+        <Suspense fallback={<div>Loading...</div>}>
+            <LogoutComponent />
+        </Suspense>
+    );
 }
